af950c751d7d332962ce95292763482fbd8b0315
JanioSantosSilva <jose.j.santos.silva@avanade.com>
2024-10-30 11:59:22 -0300

    teste name

--- test_channel.tf
+++ test_channel.tf
@@ -2,14 +2,34 @@ locals {
   # Definindo o valor do canal com acentos e espaços para teste
   channel_raw = "Mobile Cartões"
   
-  # Normalizando o canal
+  # Normalizando o canal: remove espaços e acentos
   channel_normalized = lower(
     replace(
-      replace(local.channel_raw, " ", ""),
-      "/[áàâãäéèêëíìîïóòôõöúùûüçÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÕÖÚÙÛÜÇ]/",
-      "aeeeiioouu"
+      replace(
+        replace(
+          replace(
+            replace(
+              replace(
+                replace(
+                  replace(
+                    replace(
+                      replace(
+                        replace(
+                          replace(
+                            replace(
+                              local.channel_raw,
+                              " ", ""),  # Remove espaços
+                              "á", "a"),
+                              "ã", "a"),
+                              "â", "a"),
+                              "é", "e"),
+                              "ê", "e"),
+                              "í", "i"),
+                              "ó", "o"),
+                              "ô", "o"),
+                              "ú", "u"),
+                              "ç", "c")
     )
-  )
 }
 
 output "normalized_channel" {
