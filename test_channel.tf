266ccbf51db0b639cad3555daee3830f4b6ec8c9
JanioSantosSilva <jose.j.santos.silva@avanade.com>
2024-10-30 12:05:37 -0300

    add refactor name

--- test_channel.tf
+++ test_channel.tf
@@ -2,15 +2,52 @@ locals {
   # Definindo o valor do canal com acentos e espaços para teste
   channel_raw = "Mobile Cartões"
   
-  # Normalizando o canal
+  # Normalizando o canal: remove espaços e acentos
   channel_normalized = lower(
     replace(
-      replace(local.channel_raw, " ", ""),
-      "/[áàâãäéèêëíìîïóòôõöúùûüçÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÕÖÚÙÛÜÇ]/",
+      replace(
+        replace(
+          replace(
+            replace(
+              replace(
+                replace(
+                  replace(
+                    replace(
+                      replace(
+                        replace(
+                          replace(
+                            replace(
+                              replace(
+                                replace(
+                                  replace(
+                                    replace(
+                                      replace(
+                                        local.channel_raw,
+                                        " ", ""),  # Remove espaços
+                                        "á", "a"),  # Remove acentos
+                                        "ã", "a"),
+                                        "â", "a"),
+                                        "é", "e"),
+                                        "ê", "e"),
+                                        "í", "i"),
+                                        "ó", "o"),
+                                        "ô", "o"),
+                                        "ú", "u"),
+                                        "ç", "c"),
+                                        "Á", "a"),
+                                        "À", "a"),
+                                        "Â", "a"),
+                                        "Ã", "a"),
+                                        "É", "e"),
+                                        "È", "e"),
+                                        "Í", "i"),
+                                        "Ó", "o"),
+                                        "Ô", "o"),
+                                        "Ú", "u")
     )
-  )
 }
 
 output "normalized_channel" {
   value = local.channel_normalized
 }
+
