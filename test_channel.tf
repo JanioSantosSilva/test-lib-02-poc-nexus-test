bd0c675566cbef7c85cfdff60b7052957bea9ada
JanioSantosSilva <jose.j.santos.silva@avanade.com>
2024-10-30 13:06:44 -0300

    add

--- test_channel.tf
+++ test_channel.tf
@@ -1,55 +1,45 @@
 locals {
-    # Definindo o valor do canal com acentos e espaços
-    channel_raw = "Mobile Cartões"
+  # Definindo o valor do canal com acentos e espaços
+  channel_raw = "Mobile Cartões"
 
-    # Normalizando o canal: remove espaços e acentos
-    channel_normalized = replace(
+  # Normalizando o canal: remove espaços e acentos
+  channel_normalized = replace(
+    replace(
+       replace(
         replace(
+          replace(
             replace(
+              replace(
                 replace(
+                  replace(
                     replace(
+                      replace(
                         replace(
+                          replace(
                             replace(
+                              replace(
                                 replace(
-                                    replace(
-                                        replace(
-                                            replace(
-                                                replace(
-                                                    replace(
-                                                        replace(
-                                                            replace(
-                                                                replace(
-                                                                    replace(
-                                                                        replace(
-                                                                            replace(
-                                                                                replace(
-                                                                                    replace(
-                                                                                        replace(
-                                                                                            replace(local.channel_raw, " ", ""),  # Remove espaços
-                                                                                            "á", "a"),  # Remove acento
-                                                                                        "ã", "a"),
-                                                                                    "â", "a"),
-                                                                                "é", "e"),
-                                                                            "ê", "e"),
-                                                                        "í", "i"),
-                                                                    "ó", "o"),
-                                                                "ô", "o"),
-                                                            "ú", "u"),
-                                                        "ç", "c"),
-                                                    "Á", "A"),  # Mantém capitalização
-                                                "À", "A"),
-                                            "Â", "A"),
-                                        "Ã", "A"),
-                                    "É", "E"),
-                                "È", "E"),
-                            "Í", "I"),
-                        "Ó", "O"),
-                    "Ô", "O"),
-                "Ú", "U")
-        )
-    )
+                                  replace(
+                                    local.channel_raw, " ", ""),  # Remove espaços
+                                    "á", "a"),  # Remove acento
+                                    "ã", "a"),
+                                    "â", "a"),
+                                    "é", "e"),
+                                    "ê", "e"),
+                                    "í", "i"),
+                                    "ó", "o"),
+                                    "ô", "o"),
+                                    "ú", "u"),
+                                    "ç", "c"),
+                                    "Á", "A"),  # Mantém capitalização
+                                    "À", "A"),
+                                    "Â", "A"),
+                                    "Ã", "A"),
+                                    "É", "E")
+  )
+                                  
 }
 
 output "normalized_channel" {
-    value = local.channel_normalized
+  value = local.channel_normalized
 }
