c9d803c233abfba8c842a980bbaae25959ba1279
JanioSantosSilva <jose.j.santos.silva@avanade.com>
2024-10-30 12:18:05 -0300

    add teste

--- test_channel.tf
+++ test_channel.tf
@@ -1,54 +1,16 @@
 locals {
   # Definindo o valor do canal com acentos e espaços
-  channel_raw = payload.channel
+  channel_raw = "Mobile Cartões"
   
+
   # Normalizando o canal: remove espaços e acentos
-  channel_normalized = replace(
-    replace(
-      replace(
-        replace(
-          replace(
-            replace(
-              replace(
-                replace(
-                  replace(
-                    replace(
-                      replace(
-                        replace(
-                          replace(
-                            replace(
-                              replace(
-                                replace(
-                                  replace(
-                                    replace(
-                                      local.channel_raw,
-                                      " ", ""),  # Remove espaços
-                                      "á", "a"),  # Remove acentos
-                                      "ã", "a"),
-                                      "â", "a"),
-                                      "é", "e"),
-                                      "ê", "e"),
-                                      "í", "i"),
-                                      "ó", "o"),
-                                      "ô", "o"),
-                                      "ú", "u"),
-                                      "ç", "c"),
-                                      "Á", "a"),
-                                      "À", "a"),
-                                      "Â", "a"),
-                                      "Ã", "a"),
-                                      "É", "e"),
-                                      "È", "e"),
-                                      "Í", "i"),
-                                      "Ó", "o"),
-                                      "Ô", "o"),
-                                      "Ú", "u")
+  channel_normalized = regex_replace(
+    regex_replace(local.channel_raw, "[ ]", ""),  # Remove espaços
+    "[áàâãäéèêëíìîïóòôõöúùûüçÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÕÖÚÙÛÜÇ]", 
+    "" 
   )
 }
 
-globals "terraform" "repository" "variables" {
-  CHANNEL = local.channel_normalized
-}
 
 output "normalized_channel" {
   value = local.channel_normalized
